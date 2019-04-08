9ad7bda7a0eb903ebf2ef2aac7a9d9c33ce6af70
H S <gkskfhdlstm@naver.com>
2019-04-08 15:16:19 +0900

    계산기 구현 완료

--- app/src/main/java/com/gkskfhdlstmapk/hanpinetree/bcsd_mvcpattern/CalcActivity.java
+++ app/src/main/java/com/gkskfhdlstmapk/hanpinetree/bcsd_mvcpattern/CalcActivity.java
@@ -8,9 +8,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CalcActivity extends Fragment {
+import java.util.StringTokenizer;
+
+public class CalcActivity extends Fragment implements View.OnClickListener {
     View mView;
+    private StringBuilder mUpdateText;
+    private TextView mUpdateTextview;
+
+    private Button[] mButton = new Button[10];
+    private Button mButtonResult;
+    private Button mButtonCE;
+
+    private Button mButtonPlus;
+    private Button mButtonMinus;
+    private Button mButtonMulti;
+    private Button mButtonDiv;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +42,142 @@ public class CalcActivity extends Fragment {
     }
 
     public void init(){
+        for(int i = 0 ; i < 10; i++){
+            String buttonID = "activity_calc_"+i+"_button";
+            int resID = getResources().getIdentifier(buttonID, "id","com.gkskfhdlstmapk.hanpinetree.bcsd_mvcpattern");
+            mButton[i] = mView.findViewById(resID);
+            mButton[i].setOnClickListener(this);
+        }
+        mButtonResult = (Button)mView.findViewById(R.id.activity_calc_result_button);
+        mButtonCE = (Button)mView.findViewById(R.id.activity_calc_CE_button);
 
+        mButtonPlus = (Button)mView.findViewById(R.id.activity_calc_plus_button);
+        mButtonMinus = (Button)mView.findViewById(R.id.activity_calc_minus_button);
+        mButtonMulti = (Button)mView.findViewById(R.id.activity_calc_multi_button);
+        mButtonDiv = (Button)mView.findViewById(R.id.activity_calc_div_button);
+
+        mUpdateText = new StringBuilder("");
+        mUpdateTextview = (TextView)mView.findViewById(R.id.activity_calc_result_textview);
+        mUpdateTextview.setText(mUpdateText);
+        mButtonResult.setOnClickListener(this);
+        mButtonCE.setOnClickListener(this);
+
+        mButtonPlus.setOnClickListener(this);
+        mButtonMinus.setOnClickListener(this);
+        mButtonMulti.setOnClickListener(this);
+        mButtonDiv.setOnClickListener(this);
+
+    }
+
+
+    public int calculaeResult(StringBuilder str){
+
+        StringTokenizer st = new StringTokenizer(str.toString());
+        int result = 0;
+        int count = 0;
+        int a = 0; int b; char op;
+        while(st.hasMoreTokens()) {
+            if(count == 0) a = Integer.parseInt(st.nextToken());
+            else a = result;
+
+            op = st.nextToken().charAt(0);
+            if(op == '=') break;
+            b = Integer.parseInt(st.nextToken());
+            result = calc(a,op,b);
+            count++;
+        }
+        return result;
+
+
+    }
+    public static int calc(int a, char op, int b) {
+        switch(op) {
+            case '+':
+                return a+b;
+            case '-':
+                return a-b;
+            case 'x':
+                return a*b;
+            case '/':
+                return a/b;
+        }
+        return -1;
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activity_calc_0_button:
+                mUpdateText.append("0");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_1_button:
+                mUpdateText.append("1");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_2_button:
+                mUpdateText.append("2");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_3_button:
+                mUpdateText.append("3");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_4_button:
+                mUpdateText.append("4");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_5_button:
+                mUpdateText.append("5");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_6_button:
+                mUpdateText.append("6");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_7_button:
+                mUpdateText.append("7");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_8_button:
+                mUpdateText.append("8");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_9_button:
+                mUpdateText.append("9");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            //activity_calc_plus_button
+            case R.id.activity_calc_plus_button:
+                mUpdateText.append(" + ");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_minus_button:
+                mUpdateText.append(" - ");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_multi_button:
+                mUpdateText.append(" x ");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_div_button:
+                mUpdateText.append(" / ");
+                mUpdateTextview.setText(mUpdateText);
+                break;
+
+            case R.id.activity_calc_CE_button:
+                mUpdateText.setLength(0);
+                mUpdateTextview.setText(mUpdateText);
+                break;
+            case R.id.activity_calc_result_button:
+                int calResult = calculaeResult(mUpdateText);
+                mUpdateText.setLength(0);
+                mUpdateText.append(Integer.toString(calResult));
+                mUpdateTextview.setText(calResult+"");
+                break;
+
+        }
     }
 }
